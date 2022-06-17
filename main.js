4f4543c3003ed19b09ad358035ba62607dab698c
Tejashree <sakharetejashree@gmail.com>
2022-06-17 21:09:07 +0530

    init

---
+++ main.js
@@ -0,0 +1,14 @@
+function calc(){
+    var totalemployees = document.getElementById("employees").value;
+    var totalemployees = parseInt(totalemployees,10);
+    var totalWorkdays = document.getElementById("workdays").value;
+    var totalWorkdays = parseInt(totalWorkdays,10);
+    var totalDaysAbsent = document.getElementById("absent").value;
+    var totalDaysAbsent = parseInt(totalDaysAbsent,10);
+
+    var multiplication = (totalemployees*totalWorkdays );
+    var absenceRate =(totalDaysAbsent /multiplication )* 100;
+
+    document.getElementById("rate").value =absenceRate.toFixed(4);
+
+}
